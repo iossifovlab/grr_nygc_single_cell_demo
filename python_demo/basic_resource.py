2b8be0ddf4bcc244137e0c5e8cd9e6621dd8d8b0
Ivan Iossifov <iossifov@cshl.edu>
2026-07-10 09:20:12 -0400

    started a python demo

---
+++ python_demo/basic_resource.py
@@ -0,0 +1,47 @@
+import pathlib
+import pandas as pd
+from typing import cast
+
+from gain.genomic_resources.repository_factory import build_genomic_resource_repository
+from gain.genomic_resources.genomic_scores import build_score_from_resource_id
+from gain.genomic_resources.genomic_scores import PositionScore
+
+
+
+## to get connected to *this* repository
+local_repo_path = str(pathlib.Path(__file__).absolute().parent.parent)
+grr_def = {
+    "id": "local",
+    "type": "directory",
+    "directory": local_repo_path
+}
+grr = build_genomic_resource_repository(definition=grr_def)
+
+## alternativealy to get connected to the configured repository
+# grr = build_genomic_resource_repository()
+
+
+## Find all resources for a cell-type
+for r in grr.get_all_resources():
+    if r.get_labels().get("cell_type") == "Astro":
+        print(r.resource_id)
+
+## Using basic resource
+basic_resource_id = "summary/zemke2023Conserved/cell_expression/by_sample/M1_Peregrine_rep1"
+basic_resource = grr.get_resource(basic_resource_id)
+features = pd.read_csv(basic_resource.get_file_url("M1_Peregrine_rep1_features.tsv.gz"), sep="\t")
+print(features)
+
+
+## Using basic resource
+score_id = "summary/zemke2023Conserved/pseudo_bulk_atac_bw/human_m1_ASC"
+score = cast(PositionScore, build_score_from_resource_id(score_id, grr).open())
+
+xs = []
+ys = []
+for pos_begin, pos_end, values in score.fetch_region("chr1", 151_402_724, 151_459_494):
+    if values is not None:
+        xs += range(pos_begin, pos_end + 1)
+        ys += [values[0]]*(pos_end - pos_begin + 1)
+
+
