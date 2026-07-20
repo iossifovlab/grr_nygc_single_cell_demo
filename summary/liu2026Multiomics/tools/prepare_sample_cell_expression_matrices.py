e3742e822b83fccc08a9d486e4e72bf935168e93
Ivan Iossifov <iossifov@cshl.edu>
2026-07-20 14:49:17 -0400

    built /prepare_sample_cell_expression_matrices.py for liu2026Multiomic

--- summary/liu2026Multiomics/tools/prepare_sample_cell_expression_matrices.py
+++ summary/liu2026Multiomics/tools/prepare_sample_cell_expression_matrices.py
@@ -5,12 +5,16 @@ from prepare_resrouce import prepapre_resrouce
 
 yoonHaDD = "/gpfs/commons/groups/iossifov_lab/ylee/GRR/HDMA/ATAC_RNA"
 
-for ff in glob.glob(f"{yoonHaDD}/*.fragments.tsv.gz"):
-    iff = ff + ".tbi"
-    assert os.path.isfile(iff)
-
+for ff in glob.glob(f"{yoonHaDD}/*.matrix.mtx.gz"):
     fn = ff.split("/")[-1]
     sm_id = ff.split("/")[-1].split(".")[0]
+
+    files = []
+    for sufx in [".features.tsv.gz", ".barcodes.tsv.gz"]:
+        eff = f"{yoonHaDD}/{sm_id}{sufx}"
+        assert os.path.isfile(eff)
+        files.append(eff)
+
     print(sm_id, sm_id)
 
     tissue_sc, batch_n, organ, pcw_age = sm_id.split("_")
@@ -25,23 +29,21 @@ for ff in glob.glob(f"{yoonHaDD}/*.fragments.tsv.gz"):
 
     print(sm_id, tissue_sc, batch_n, organ, pcw_age)
 
-    resource_id = f"summary/liu2026Multiomics/sample_atac_fragments/{sm_id}"
-    files = [ff, iff]
+    resource_id = f"summary/liu2026Multiomics/sample_cell_expression_matrix/{sm_id}"
     conf = {
         "type": "basic",
-        "file": ff.split("/")[-1],
-        "index_file": iff.split("/")[-1],
+        "file": f"{sm_id}.matrix.mtx.gz",
+        "features_file": f"{sm_id}.features.tsv.gz",
+        "barcodes_file": f"{sm_id}.barcodes.tsv.gz",
         "meta": {
-            "summary": f"Sample ATAC fragments for {sm_id} from liu2026Multiomics.",
+            "summary": f"Sample cell expression matrix for {sm_id} from liu2026Multiomics.",
             "description": dedent("""
                 [Liu, et al, Multiomics and deep learning dissect regulatory syntax in human development, Nature 2026.](https://www.nature.com/articles/s41586-026-10326-9)
 
                 Downloaded from: [https://zenodo.org/records/15066651](https://zenodo.org/records/15066651).
-
-                Genomic coordinates in fragment files are 0-based.
             """),
             "labels": {
-                "assay": "atac_fragmets",
+                "assay": "cell_expression",
                 "technology": "SHARE-seq",
                 "tissue_storage_code": tissue_sc,
                 "exp_batch": batch_n,
@@ -51,5 +53,5 @@ for ff in glob.glob(f"{yoonHaDD}/*.fragments.tsv.gz"):
             }
         }
     }
-    prepapre_resrouce(resource_id, files, conf)
+    # prepapre_resrouce(resource_id, files, conf)
 
