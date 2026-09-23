54761d528d96c6bab066bcaac27456efa10f14d8
Ivan Iossifov <iossifov@cshl.edu>
2026-09-23 14:24:55 -0400

    beautified the basic_resource.py

--- python_demo/basic_resource.py
+++ python_demo/basic_resource.py
@@ -1,33 +1,17 @@
-import pandas as pd
-import numpy as np
-
-from gain.genomic_resources.repository_factory import build_genomic_resource_repository
-from gain.genomic_resources.genomic_scores import build_position_score_from_resource_id
-
+# Working with GRR
+from gain.genomic_resources.repository_factory import \
+    build_genomic_resource_repository
 
 grr = build_genomic_resource_repository()
-
-
-# # Find all resources for a cell-type
-# for r in grr.get_all_resources():
-#     if r.get_labels().get("cell_type") == "Astro":
-#         print(r.resource_id)
-
 for r in grr.search_resources("cell_type : Astro"):
-    print(r.resource_id)
+    print(r.resource_id, r.get_type(), r.get_labels())
 
 
-# Using basic resource
-basic_resource_id = "summary/zemke2023Conserved/rna_expression_atac_matrix/M1_Peregrine_rep1"
-basic_resource = grr.get_resource(basic_resource_id)
-features = pd.read_csv(basic_resource.get_file_url("M1_Peregrine_rep1_features.tsv.gz"), sep="\t")
-print(features)
+# Working with a PositionScore
+from gain.genomic_resources.genomic_scores import \
+    build_position_score_from_resource_id
 
-
-# Using position score
 score_id = "summary/zemke2023Conserved/pseudobulk_atac/human_m1/ASC"
 score = build_position_score_from_resource_id(score_id).open()
-
-
-ys = np.array(list(score.get_score_in_region("chr1", 151_402_724, 151_459_494)))
+ys = list(score.get_score_in_region("chr1", 151_403_000, 151_403_100))
 print(ys)
